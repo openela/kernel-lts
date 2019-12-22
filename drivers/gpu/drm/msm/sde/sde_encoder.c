c0f170a7445d4850d2553262ce343106416e2c4f
Danny Lin <danny@kdrag0n.dev>
2019-12-22 17:39:21 +0530

    display: Trigger panel reset after first kickoff on non-60 Hz
    If we're using a refresh rate different from what the bootloader
    initializes the display with, a panel reset is necessary for our custom
    refresh rate to take effect.
    
    After the first kickoff, request a panel reset from userspace using the
    existing ESD recovery mechanism to update the refresh rate. Note that
    this is only done if the target refresh rate is *not* 60 Hz because this
    is redundant if we don't need a refresh rate update.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I2a4dcae2043d651926929aa422ab34d655c15991
    
    [RealJohnGalt]: adapt to k4.14. Fixes rare case of locked wrong rate gamma
    on boot.

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -4867,6 +4867,7 @@ static int _sde_encoder_reset_ctl_hw(struct drm_encoder *drm_enc)
 
 void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 {
+	static bool first_run = true;
 	struct sde_encoder_virt *sde_enc;
 	struct sde_encoder_phys *phys;
 	ktime_t wakeup_time;
@@ -4893,6 +4894,25 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 		phys = sde_enc->phys_encs[i];
 		if (phys && phys->ops.handle_post_kickoff)
 			phys->ops.handle_post_kickoff(phys);
+	}
+
+	/*
+	 * Trigger a panel reset if this is the first kickoff and the refresh
+	 * rate is not 60 Hz
+	 */
+	if (cmpxchg(&first_run, true, false) &&
+		sde_enc->crtc->mode.vrefresh != 60) {
+		struct sde_connector *conn = container_of(phys->connector, struct sde_connector, base);
+		struct drm_event event = {
+			.type = DRM_EVENT_PANEL_DEAD,
+			.length = sizeof(bool)
+		};
+
+		conn->panel_dead = true;
+		event.type = DRM_EVENT_PANEL_DEAD;
+		event.length = sizeof(bool);
+		msm_mode_object_event_notify(&conn->base.base,
+			conn->base.dev, &event, (u8 *) &conn->panel_dead);
 	}
 
 	if (sde_enc->disp_info.intf_type == DRM_MODE_CONNECTOR_DSI &&
