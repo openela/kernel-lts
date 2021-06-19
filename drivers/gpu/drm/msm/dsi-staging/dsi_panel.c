1df75f553b9abd8c4d3b1b3b6d695c4b5b2308cb
Vaisakh Murali <mvaisakh@statixos.com>
2021-06-19 17:12:25 +0500

    drivers: gpu: drm-msm: Enforce ULPS Suspend feature as well
    * We already enforce ulps, might as well enforce this.
    * This allows the panel to enter the ulps state during suspend.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2702,8 +2702,7 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	pr_info("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled =
-		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
+	panel->ulps_suspend_enabled = true;
 
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
