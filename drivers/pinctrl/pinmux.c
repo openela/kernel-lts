28225973c22043b87d48b2b5804836edd461cbf9
karthick111 <karthickrko61@gmail.com>
2021-11-20 09:27:06 +0530

    drivers: pinctrl: Import Realme changes
    Change-Id: Ia96b382581833c37f37685cd69d4d91e3a32638a

--- drivers/pinctrl/pinmux.c
+++ drivers/pinctrl/pinmux.c
@@ -501,11 +501,22 @@ void pinmux_disable_setting(const struct pinctrl_setting *setting)
 
 			gname = pctlops->get_group_name(pctldev,
 						setting->data.mux.group);
+#ifndef OPLUS_FEATURE_CHG_BASIC
+/* LiYue@BSP.CHG.Basic, 2019/09/12, Modify for printk rate */
 			dev_warn(pctldev->dev,
 				 "not freeing pin %d (%s) as part of "
 				 "deactivating group %s - it is already "
 				 "used for some other setting",
 				 pins[i], desc->name, gname);
+#else
+			if (printk_ratelimit()) {
+				dev_warn(pctldev->dev,
+					"not freeing pin %d (%s) as part of "
+					"deactivating group %s - it is already "
+					"used for some other setting",
+					pins[i], desc->name, gname);
+			}
+#endif
 		}
 	}
 }
