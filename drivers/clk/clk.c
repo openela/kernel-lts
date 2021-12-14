f5ee04b1a0c9c07b0aa00cf5cd6106623c568c75
karthick111 <karthickrko61@gmail.com>
2021-12-14 19:31:24 +0530

    drivers: clk: Import Realme changes
    Change-Id: I59e11c8b5ad6a8c3f01533e9fa4d130b3cc46b2c

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2695,7 +2695,12 @@ EXPORT_SYMBOL_GPL(clk_set_flags);
 
 static struct dentry *rootdir;
 static int inited = 0;
+#ifndef OPLUS_FEATURE_POWERINFO_STANDBY
+//Nanwei.Deng@BSP.CHG.Basic 2018/05/03 modify for power debug
 static u32 debug_suspend;
+#else
+static u32 debug_suspend = 1;
+#endif /* VENDOR_EDIT */
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
 
