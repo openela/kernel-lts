9aff500c1059712ee0b52c7d68c61789a14fb97c
karthick111 <karthickrko61@gmail.com>
2021-12-15 12:58:11 +0530

    fs: pstore: Import Realme changes
    Change-Id: Ia758e641c2a8a5dc85dfd93909b9a701ed70bff5

--- include/linux/pstore_ram.h
+++ include/linux/pstore_ram.h
@@ -98,6 +98,10 @@ struct ramoops_platform_data {
 	unsigned long	console_size;
 	unsigned long	ftrace_size;
 	unsigned long	pmsg_size;
+#ifdef OPLUS_FEATURE_DUMPDEVICE
+//zhangzongyu@BSP.Kernel.Stability, 2020/05/10, Add for dump device info
+	unsigned long	device_info_size;
+#endif /* OPLUS_FEATURE_DUMPDEVICE */
 	int		dump_oops;
 	u32		flags;
 	struct persistent_ram_ecc_info ecc_info;
