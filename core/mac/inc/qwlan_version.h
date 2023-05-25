312e5c09e8c0c3d0aca72f496f4d4ad4bfb4fca5
Rahul Choudhary <quic_rahulc@quicinc.com>
2023-05-25 04:28:21 -0700

    Release 5.2.022.12
    Release 5.2.022.12
    
    Change-Id: Ieffdc38e6a0ac6a72a6268dcb68a56ffbec7ec3c
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            022
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            11
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            12
 
-#define QWLAN_VERSIONSTR               "5.2.022.11Z"
+#define QWLAN_VERSIONSTR               "5.2.022.12"
 
 #endif /* QWLAN_VERSION_H */
